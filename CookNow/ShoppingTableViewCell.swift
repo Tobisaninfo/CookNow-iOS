b147c29a49b6e0b4f91f5dfdbcff30a37bdae5e1
tobias <thinkdifferent055@gmail.com>
2017-06-10 19:23:59 +0200

    Add Shopping Tab

---
+++ CookNow/ShoppingTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ShoppingTableViewCell.swift
+//  CookNow
+//
+//  Created by Tobias on 10.06.17.
+//  Copyright © 2017 Tobias. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CookNow/ShoppingViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ShoppingViewController.swift
+//  CookNow
+//
+//  Created by Tobias on 10.06.17.
+//  Copyright © 2017 Tobias. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //tableView.contentInset.top = 20
+        //tableView.contentInset.bottom = 100
+        // Do any additional setup after loading the view.
+        tableView.register(UINib(nibName: "ShoppingTableViewCell", bundle: Bundle.main), forCellReuseIdentifier: "shoppingCell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingCell", for: indexPath) as! ShoppingTableViewCell
+        cell.nameLabel?.text = "Schokolade"
+        cell.descriptionLabel?.text = "500g"
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if let type = tableView.cellForRow(at: indexPath)?.accessoryType {
+            if type == .checkmark {
+                tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            } else {
+                tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            }
+        }
+        return indexPath
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}

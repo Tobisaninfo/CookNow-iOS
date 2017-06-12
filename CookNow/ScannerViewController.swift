944ad475643e6c8f7311e251b6f6c16f7ca556b6
tobias <thinkdifferent055@gmail.com>
2017-06-12 22:32:15 +0200

    Add Barcode Scanner, Camera Touch Toggle Button

---
+++ CookNow/ScannerViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ScannerViewController.swift
+//  CookNow
+//
+//  Created by Tobias on 12.06.17.
+//  Copyright © 2017 Tobias. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    @IBOutlet weak var flashlightButton: UIButton!
+    
+    var captureDevice: AVCaptureDevice?
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    
+    let supportedCodeTypes = [AVMetadataObjectTypeUPCECode,
+                              AVMetadataObjectTypeCode39Code,
+                              AVMetadataObjectTypeCode39Mod43Code,
+                              AVMetadataObjectTypeCode93Code,
+                              AVMetadataObjectTypeCode128Code,
+                              AVMetadataObjectTypeEAN8Code,
+                              AVMetadataObjectTypeEAN13Code,
+                              AVMetadataObjectTypeAztecCode,
+                              AVMetadataObjectTypePDF417Code]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video as the media type parameter.
+        captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Initialize the captureSession object.
+            captureSession = AVCaptureSession()
+            
+            // Set the input device on the capture session.
+            captureSession?.addInput(input)
+            
+            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+            
+            // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            
+            // Start video capture.
+            captureSession?.startRunning()
+            
+            view.bringSubview(toFront: flashlightButton)
+        } catch {
+            // If any error occurs, simply print it out and don't continue any more.
+            print(error)
+            // TODO handle user error
+            return
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        captureSession?.stopRunning()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - AVCaptureMetadataOutputObjectsDelegate Methods
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            print("No QR/barcode is detected")
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if supportedCodeTypes.contains(metadataObj.type) {
+            if metadataObj.stringValue != nil {
+                print(metadataObj.stringValue)
+
+            }
+        }
+    }
+    
+    private var flashOn = false
+    
+    @IBAction func flashlightHandler(_ sender: Any) {
+        if let captureDevice = self.captureDevice {
+            if captureDevice.hasFlash && captureDevice.hasTorch {
+                do {
+                    try captureDevice.lockForConfiguration()
+                    
+                    if flashOn {
+                        captureDevice.torchMode = AVCaptureTorchMode.off
+                    } else {
+                        try captureDevice.setTorchModeOnWithLevel(AVCaptureMaxAvailableTorchLevel)
+                    }
+                    flashOn = !flashOn
+                    captureDevice.unlockForConfiguration()
+                } catch {
+                    // TODO handle user error
+                    print(error)
+                }
+            }
+        }
+    }
+}
